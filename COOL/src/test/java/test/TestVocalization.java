51774169d0f371587752d1d1c0e1ef439561aa86
enricogallinucci <e.gallinucci@gmail.com>
2021-09-20 10:11:51 +0200

    mckp tests added

--- COOL/src/test/java/test/TestVocalization.java
+++ COOL/src/test/java/test/TestVocalization.java
@@ -25,6 +25,9 @@ public class TestVocalization {
         assertEquals(Sets.newHashSet(a, c), Optimizer.getDummyPatterns(Sets.newHashSet(p1, p2),20));
     }
 
+    /**
+     * Only take one item for each group; A is taken instead of B
+     */
     @Test
     public void test1() {
         final VocalizationPattern a = new VocalizationPattern("foo", 0.8, 3, "foo", PatternState.AVAILABLE);
@@ -35,6 +38,9 @@ public class TestVocalization {
         assertEquals(Sets.newHashSet(a, c), Optimizer.getPatterns(Sets.newHashSet(p1, p2), 20));
     }
 
+    /**
+     * Do not exceed bag size; only A is taken
+     */
     @Test
     public void test2() {
         final VocalizationPattern a = new VocalizationPattern("foo", 0.8, 3, "foo", PatternState.AVAILABLE);
@@ -43,5 +49,30 @@ public class TestVocalization {
         final Set<IVocalizationPattern> p1 = Sets.newHashSet(a, b);
         final Set<IVocalizationPattern> p2 = Sets.newHashSet(c);
         assertEquals(Sets.newHashSet(a), Optimizer.getPatterns(Sets.newHashSet(p1, p2), 4));
+    }
+
+    /**
+     * No item is taken
+     */
+    @Test
+    public void test3() {
+        final VocalizationPattern a = new VocalizationPattern("foo", 0.8, 3, "foo", PatternState.AVAILABLE);
+        final VocalizationPattern b = new VocalizationPattern("bar", 0.7, 3, "foo", PatternState.AVAILABLE);
+        final VocalizationPattern c = new VocalizationPattern("foobar", 0.6, 6, "foo", PatternState.AVAILABLE);
+        final Set<IVocalizationPattern> p1 = Sets.newHashSet(a, b);
+        final Set<IVocalizationPattern> p2 = Sets.newHashSet(c);
+        assertEquals(Sets.newHashSet(), Optimizer.getPatterns(Sets.newHashSet(p1, p2), 2));
+    }
+
+    /**
+     * Determinism; return the first in case of tie
+     */
+    @Test
+    public void test4() {
+        final VocalizationPattern a = new VocalizationPattern("foo", 0.8, 3, "foo", PatternState.AVAILABLE);
+        final VocalizationPattern b = new VocalizationPattern("bar", 0.8, 3, "foo", PatternState.AVAILABLE);
+        final Set<IVocalizationPattern> p1 = Sets.newHashSet(a);
+        final Set<IVocalizationPattern> p2 = Sets.newHashSet(b);
+        assertEquals(Sets.newHashSet(a), Optimizer.getPatterns(Sets.newHashSet(p1, p2), 5));
     }
 }
