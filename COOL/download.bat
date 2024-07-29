74b5f1c6f19e4b51012f948be91bc7c1491899fb
Matteo Francia <m.francia@unibo.it>
2024-07-29 17:50:37 +0200

    working on the ci

--- COOL/download.bat
+++ COOL/download.bat
@@ -1,5 +1,5 @@
 cd resources
-curl -oL foodmart-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
-curl -oL covid_weekly-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/covid_weekly-mysql.sql
-curl -oL ssb_test-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/ssb_test-mysql.sql
+curl -L -o  foodmart-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
+curl -L -o  covid_weekly-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/covid_weekly-mysql.sql
+curl -L -o  ssb_test-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/ssb_test-mysql.sql
 cd ..
