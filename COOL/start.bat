212e4908e0c3cca697d5342ba50fc3bbc6ddb53c
Matteo Francia <m.francia@unibo.it>
2025-01-08 10:21:56 +0100

    ci: replace docker-compose with docker compose

--- COOL/start.bat
+++ COOL/start.bat
@@ -1,2 +1,2 @@
-docker-compose down
-docker-compose up --build -d
+docker compose down
+docker compose up --build -d
--- COOL/start.sh
+++ COOL/start.sh
@@ -4,5 +4,5 @@ if [ -f .env ]; then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 ./stop.sh
-docker-compose up --build -d
+docker compose up --build -d
 ./wait-for-it.sh ${MYSQL_URL}:${MYSQL_PORT} --strict --timeout=10 -- echo "MySQL is up"
